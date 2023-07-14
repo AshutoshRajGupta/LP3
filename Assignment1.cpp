d16de5cdee3c40a6c2284d7b805d10db5c08c2b3
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-07-14 20:57:49 +0530

    assignment 1 added

---
+++ Assignment1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int fibonacciIterative(int n) {
+    if (n <= 1)
+        return n;
+
+    int prev2 = 0;
+    int prev1 = 1;
+    for (int i = 2; i <= n; ++i) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+    }
+    return prev1;
+}
+
+// Recursive Fibonacci function
+int fibonacciRecursive(int n) {
+    if (n <= 1)
+        return n;
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+int main() {
+    int n = 10;
+    cout << "Fibonacci(" << n << ") using iterative method: " << fibonacciIterative(n) << endl;
+    cout << "Fibonacci(" << n << ") using recursive method: " << fibonacciRecursive(n) << endl;
+    return 0;
+}
