818e07bbbc6bf49592dc41a81e20659e2c1c203a
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-09-14 00:04:52 +0530

    updated assignments

--- GroupA/Assignment5.cpp
+++ GroupA/Assignment5.cpp
@@ -1,46 +1,77 @@
 #include <iostream>
 #include <vector>
+using namespace std;
 
-bool isSafe(const std::vector<int>& board, int row, int col) {
+void printQueens(const vector<int>& board) {
+    int n = board.size();
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (board[i] == j) cout << "Q ";
+            else cout << "* ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+// Function to check if it's safe to place a queen at the given position
+bool isSafe(const vector<int>& board, int row, int col) {
+    int n = board.size();
     for (int i = 0; i < row; i++) {
-        if (board[i] == col || board[i] == col - (row - i) || board[i] == col + (row - i)) {
+        if (board[i] == col || 
+            abs(board[i] - col) == abs(i - row)) {
             return false;
         }
     }
     return true;
 }
 
-void solveNQueens(std::vector<int>& board, int row, int n, int& count) {
+// Backtracking function to find N-Queens solution
+bool solveNQueens(vector<int>& board, int row) {
+    int n = board.size();
+    // If all queens are placed, return true
     if (row == n) {
-        count++;
-        return;
+        printQueens(board);
+        return true;
     }
-
+    bool found = false;
+    // Try placing a queen in each column of the current row
     for (int col = 0; col < n; col++) {
-        if (col == board[0]) {
-            continue;  // Skip the column of the first queen
-        }
-
         if (isSafe(board, row, col)) {
             board[row] = col;
-            solveNQueens(board, row + 1, n, count);
+            found |= solveNQueens(board, row + 1);
+            board[row] = -1; // Backtrack
         }
     }
+    return found;
 }
 
 int main() {
     int n;
-    std::cout << "Enter the number of queens (N): ";
-    std::cin >> n;
+    cout << "Enter the value of n: ";
+    cin >> n;
 
-    std::vector<int> board(n, -1);
-    std::cout << "Enter the column position of the first queen (0-based indexing): ";
-    std::cin >> board[0];
+    if (n <= 0) {
+        cout << "Invalid input. Please enter a positive integer." << endl;
+        return 1;
+    }
 
-    int count = 0;
-    solveNQueens(board, 1, n, count);
+    vector<int> board(n, -1); // Initialize the board with -1 (no queens placed yet)
+    
+    int firstQueenColumn;
+    cout << "Enter the column (0-indexed) for the first queen: ";
+    cin >> firstQueenColumn;
 
-    std::cout << "Total solutions: " << count << std::endl;
+    if (firstQueenColumn < 0 || firstQueenColumn >= n) {
+        cout << "Invalid input for the first queen's column." << endl;
+        return 1;
+    }
+
+    board[0] = firstQueenColumn; // Place the first queen
+
+    if (!solveNQueens(board, 1)) {
+        cout << "No solution found." << endl;
+    }
 
     return 0;
 }
