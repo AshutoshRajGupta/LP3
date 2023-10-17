69aed8fb1e7d7806de44e130abfe7f7873dcf338
Ashutosh Raj Gupta <ag2364443@gmail.com>
2023-10-18 03:27:36 +0530

    updated

--- GroupA/Assignment1.cpp
+++ GroupA/Assignment1.cpp
@@ -1,24 +1,4 @@
-// #include <iostream>
-// using namespace std;
-// int fibonacciIterative(int n) {
-//     if (n <= 1)
-//         return n;
 
-//     int prev2 = 0;
-//     int prev1 = 1;
-//     for (int i = 2; i <= n; ++i) {
-//         int curr = prev1 + prev2;
-//         prev2 = prev1;
-//         prev1 = curr;
-//     }
-//     return prev1;
-// }
-// // Recursive Fibonacci function
-// int fibonacciRecursive(int n) {
-//     if (n <= 1)
-//         return n;
-//     return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
-// }
 
 // int main() {
 //     int n;
--- GroupA/Assignment5.cpp
+++ GroupA/Assignment5.cpp
@@ -2,76 +2,55 @@
 #include <vector>
 using namespace std;
 
-void printQueens(const vector<int>& board) {
-    int n = board.size();
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (board[i] == j) cout << "Q ";
-            else cout << "* ";
-        }
-        cout << endl;
-    }
-    cout << endl;
-}
-
-// Function to check if it's safe to place a queen at the given position
-bool isSafe(const vector<int>& board, int row, int col) {
-    int n = board.size();
+bool isSafe(vector<int>& board, int row, int col) {
     for (int i = 0; i < row; i++) {
-        if (board[i] == col || 
-            abs(board[i] - col) == abs(i - row)) {
+        if (board[i] == col || board[i] - i == col - row || board[i] + i == col + row) {
             return false;
         }
     }
     return true;
 }
 
-// Backtracking function to find N-Queens solution
-bool solveNQueens(vector<int>& board, int row) {
-    int n = board.size();
-    // If all queens are placed, return true
+void solveNQueens(vector<int>& board, int row, int n, int& count) {
     if (row == n) {
-        printQueens(board);
-        return true;
+        count++;
+        cout << "Solution " << count << ":\n";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << (board[i] == j ? "Q " : ". ");
+            }
+            cout << endl;
+        }
+        cout << endl;
+        return;
     }
-    bool found = false;
-    // Try placing a queen in each column of the current row
+
     for (int col = 0; col < n; col++) {
         if (isSafe(board, row, col)) {
             board[row] = col;
-            found |= solveNQueens(board, row + 1);
-            board[row] = -1; // Backtrack
+            solveNQueens(board, row + 1, n, count);
         }
     }
-    return found;
+}
+
+void solveNQueens(int n) {
+    vector<int> board(n, -1);
+    int count = 0;
+    solveNQueens(board, 0, n, count);
+
+    if (count == 0) {
+        cout << "No solutions found.\n";
+    } else {
+        cout << "Total solutions: " << count << endl;
+    }
 }
 
 int main() {
     int n;
-    cout << "Enter the value of n: ";
+    cout << "Enter the number of queens (N): ";
     cin >> n;
 
-    if (n <= 0) {
-        cout << "Invalid input. Please enter a positive integer." << endl;
-        return 1;
-    }
-
-    vector<int> board(n, -1); // Initialize the board with -1 (no queens placed yet)
-    
-    int firstQueenColumn;
-    cout << "Enter the column (0-indexed) for the first queen: ";
-    cin >> firstQueenColumn;
-
-    if (firstQueenColumn < 0 || firstQueenColumn >= n) {
-        cout << "Invalid input for the first queen's column." << endl;
-        return 1;
-    }
-
-    board[0] = firstQueenColumn; // Place the first queen
-
-    if (!solveNQueens(board, 1)) {
-        cout << "No solution found." << endl;
-    }
+    solveNQueens(n);
 
     return 0;
 }
